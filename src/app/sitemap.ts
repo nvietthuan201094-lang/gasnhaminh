76ccc5ed0f32783f5a830ec800c527f46ee89f6d
Nguyen Viet Thuan <nvietthuan201094@gmail.com>
2026-08-31 16:05:55 +0700

    Update website gasnhaminh

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  // Thay thế URL bằng domain thật của bạn sau khi go live
-  const baseUrl = 'https://GASO.vercel.app';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://gasnhaminh.com';
 
   return [
     {
